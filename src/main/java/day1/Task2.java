8e5e7410ca4b2034e2ac88a53073b26a1eba1f19
Scherbakov Anton <illusion-of-meaning@ya.ru>
2020-12-10 22:09:42 +0900

    Task4 is complite

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -3,7 +3,7 @@ package day1;
 public class Task2 {
     public static void main(String[] args) {
         String str = "JAVA";
-        for (int i = 0; i<10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.printf("%s ", str);
         }
     }
