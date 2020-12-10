70187183e535b4291873b79d37196f4428d77387
Scherbakov Anton <illusion-of-meaning@ya.ru>
2020-12-10 22:12:10 +0900

    Task 5 is complite

--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
-
+        int year = 1980;
+        for (; year <= 2020; year += 4){
+            System.out.printf("Олимпиада %d года%n", year);
+        }
     }
 }
