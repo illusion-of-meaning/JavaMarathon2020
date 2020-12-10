cf4e954b083fdf63ceaeb699db37a0a62ed3498f
Scherbakov Anton <illusion-of-meaning@ya.ru>
2020-12-10 22:05:28 +0900

    Task1, Task2, Task3 - complite

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        String word = "JAVA";
+        int i = 0;
+        while (i < 10) {
+            System.out.printf("%s ", word);
+            i++;
+        }
     }
 }
