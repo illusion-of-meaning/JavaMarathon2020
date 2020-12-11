bc318adb4a1274c7f0d82cecaf45946c533be61e
Scherbakov Anton <illusion-of-meaning@ya.ru>
2020-12-11 14:31:19 +0900

    Task2 in day 2 is complite

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,21 @@
 package day2;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader ( new InputStreamReader(System.in));
+        System.out.printf("1: ");
+        int a = Integer.valueOf(reader.readLine());
+        System.out.printf("2: ");
+        int b = Integer.valueOf(reader.readLine());
+        while( a <= b ){
+            if ( (a % 5 == 0) && (a % 10 != 0)){
+                System.out.printf("%d%n", a);
+            }
+            a += 1;
+        }
     }
 }
